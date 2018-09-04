0e0e67fc30b84ef7a071dc8458838f35bd786726
Minh Le <hoale@microsoft.com>
2018-09-04 12:53:58 -0700

    Add dev experiment framework

---
+++ js/ca.maidao2018/telemetry.js
@@ -0,0 +1,23 @@
+if (window.location.host === "mai-dao2018.com"
+    || window.location.host === "www.mai-dao2018.com") {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-124844037-3');
+}
+
+if (window.location.host === "maidao2018.com"
+    || window.location.host === "www.maidao2018.com") {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-124844037-1');
+}
+
+if (window.location.host === "mai-dao2018.tk"
+    || window.location.host === "www.mai-dao2018.tk") {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-124844037-2', { 'optimize_id': 'GTM-PMCM6H7' });
+}
